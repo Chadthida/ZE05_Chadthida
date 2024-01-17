80fbf3f6f980b9aecf77fd859c04f98324e83d4e
Yuttana ChuanChuChai <646051200129@mail.rmutk.ac.th>
2024-01-17 12:20:01 +0700

    ZE04

---
+++ lib/stylet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ze04/stylet.dart';
+
+class StyleT extends StatelessWidget{
+  StyleT({super.key});
+  Widget build(context){
+  return Text("Hello World", style: TextStyle(fontSize: 28, color: Colors.white));
+  }
+}
